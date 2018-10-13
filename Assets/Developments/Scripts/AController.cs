cfcdb424fbf9caa032a5fa29a3d39d036ae5a7e5
MiraOka <uaofmsbct.9g@gmail.com>
2018-10-13 20:05:38 +0900

    create fire

--- Assets/Developments/Scripts/AController.cs
+++ Assets/Developments/Scripts/AController.cs
@@ -4,13 +4,25 @@ using UnityEngine;
 
 public class AController : MonoBehaviour {
 
-	// Use this for initialization
-	void Start () {
-		
-	}
-	
-	// Update is called once per frame
-	void Update () {
-		
-	}
+    [SerializeField] GameObject[] fires;
+
+    void Update()
+    {
+        if (Input.GetKey(KeyCode.A))
+        {
+            for (int i = 0; i < fires.Length; i++)
+            {
+                fires[i].transform.localScale += new Vector3(0.01f, 0.01f, 0.01f);
+            }
+        }
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if(other.gameObject.tag == "Fire")
+        {
+            //Destroy(this.gameObject);
+            Debug.Log("OnTriggerEnter");
+        }
+    }
 }
--- Assets/Developments/Scripts/SController.cs
+++ Assets/Developments/Scripts/SController.cs
@@ -4,13 +4,30 @@ using UnityEngine;
 
 public class SController : MonoBehaviour {
 
-	// Use this for initialization
-	void Start () {
-		
-	}
-	
-	// Update is called once per frame
+    [SerializeField] GameObject[] fires;
+    [SerializeField] GameObject firePrefab;
+    bool isFirst = true;
+
 	void Update () {
-		
+        if(Input.GetKey(KeyCode.S))
+        {
+            for(int i = 0; i < fires.Length; i++)
+            {
+                fires[i].transform.localScale += new Vector3(0.01f, 0.01f, 0.01f);
+            }
+        }		
 	}
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.gameObject.tag == "FireA" && isFirst)
+        {
+            Vector3 firePos = (gameObject.transform.position + other.gameObject.transform.position) / 2;
+            Vector3 fireScale = (gameObject.transform.localScale + other.gameObject.transform.localScale) / 2;
+            GameObject fire = Instantiate(firePrefab);
+            fire.transform.localScale = fireScale;
+            fire.transform.parent = gameObject.transform.parent.parent;
+            isFirst = false;
+        }
+    }
 }
